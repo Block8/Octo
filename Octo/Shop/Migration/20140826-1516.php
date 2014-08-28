baefb80262c208eb72ed82a82b59dbbaa82113b9
Lesze Pietrzak <leszek.pietrzak@re-systems.co.uk>
2014-08-28 16:09:13 +0100

    fix country_code field length in contact table

--- Octo/Shop/Migration/20140826-1516.php
+++ Octo/Shop/Migration/20140826-1516.php
@@ -1,7 +1,7 @@
 <?php
 //LPP
 $queries = [];
-$queries[] = "ALTER TABLE `contact` CHANGE `country_code` `country_code` VARCHAR(2)  NULL  DEFAULT NULL;";
+$queries[] = "ALTER TABLE `contact` CHANGE `country_code` `country_code` VARCHAR(3)  NULL  DEFAULT NULL;";
 $queries[] = "ALTER TABLE `invoice` CHANGE `shipping_cost` `shipping_cost` DECIMAL(10,2)  NULL  DEFAULT NULL;";
 $queries[] = "ALTER TABLE `invoice` CHANGE `country_code` `country_code` VARCHAR(3)  NULL  DEFAULT NULL;";
 $queries[] = "ALTER TABLE `invoice_adjustment` ADD `gift_aid` TINYINT(1)  NOT NULL  DEFAULT '0'  AFTER `gift_aid`;";
