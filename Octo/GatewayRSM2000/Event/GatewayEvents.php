d7c0d117c9847d5058b044816bf30df841f7013c
Lesze Pietrzak <leszek.pietrzak@re-systems.co.uk>
2014-09-01 14:59:42 +0100

    Add support for UK Channel Islands - Shipping as to Europe country, but GBR to RSM2000

--- Octo/GatewayRSM2000/Event/GatewayEvents.php
+++ Octo/GatewayRSM2000/Event/GatewayEvents.php
@@ -3,6 +3,7 @@ namespace Octo\GatewayRSM2000\Event;
 
 use b8\Config;
 use b8\Form;
+use HMUK\Utilities\PostageCalculator;
 use Octo\Event\Listener;
 use Octo\Event\Manager;
 use Octo\Store;
@@ -25,6 +26,12 @@ class GatewayEvents extends Listener
         $view->billingAddress = json_decode($invoice->getBillingAddress(), true);
         $view->shippingAddress = json_decode($invoice->getShippingAddress(), true);
         $view->uniqueid = md5($invoice->getId());
+
+        $country_code = $view->billingAddress['country_code'];
+        if(PostageCalculator::isGBR($country_code)) { //Support UK Channel Islands, payment like for Europe country
+            $country_code = PostageCalculator::UK_COUNTRY_CODE;
+        }
+        $view->country_code = $country_code;
 
         /** @var \Octo\Invoicing\Model\InvoiceAdjustment[] $adjustments */
         $adjustments = Store::get('InvoiceAdjustment')->getByInvoiceId($invoice->getId());
