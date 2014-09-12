d4a3f0215aef331c8e2dd1a10f0a630376c4349c
Lesze Pietrzak <leszek.pietrzak@re-systems.co.uk>
2014-09-12 17:07:55 +0100

    Table for thankq logs

--- Octo/Shop/Migration/20140912-1300.php
+++ Octo/Shop/Migration/20140912-1300.php
@@ -5,11 +5,12 @@ $queries[] = "CREATE TABLE `thankq` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `serial_number` varchar(50) DEFAULT NULL,
   `invoice_id` int(11) unsigned DEFAULT NULL,
-  `giftaid_result` varchar(50) DEFAULT NULL,
+  `giftaid_id` varchar(50) DEFAULT NULL,
   `donation_id` varchar(50) DEFAULT NULL,
   `productgroups_amounts` text,
   `productgroups_donationids` text,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `invoice_id` (`invoice_id`),
   CONSTRAINT `thankq_ibfk_1` FOREIGN KEY (`invoice_id`) REFERENCES `invoice` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;";
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;";
