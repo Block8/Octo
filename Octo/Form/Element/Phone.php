bc9824ac863a3695a89304e93df5d0b67ef003c9
Lesze Pietrzak <leszek.pietrzak@re-systems.co.uk>
2014-09-15 16:07:55 +0100

    better Phone validation for RSM2000

--- Octo/Form/Element/Phone.php
+++ Octo/Form/Element/Phone.php
@@ -8,12 +8,12 @@ class Phone extends Text
     public function __construct($name = null)
     {
         parent::__construct($name);
-        $this->setPattern('([0-9 ]{9,})');
+        $this->setPattern('(^[0-9 \-\+\(\)]{9,15}$)');
     }
 
     public function setValue($value)
     {
-        $value = preg_replace('/([^0-9])/', '', $value);
+        $value = preg_replace('/([^0-9\-\+\(\)])/', '', $value);
         parent::setValue($value);
     }
 }
