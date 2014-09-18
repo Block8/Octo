8e1051e717426c3e40b8fa034d5b9ce8d2b046ae
Lesze Pietrzak <leszek.pietrzak@re-systems.co.uk>
2014-09-18 16:32:57 +0100

    get invoice already generated for basketid and contactId and is not paid

--- Octo/Invoicing/Store/InvoiceStore.php
+++ Octo/Invoicing/Store/InvoiceStore.php
@@ -68,6 +68,26 @@ class InvoiceStore extends Octo\Store
         }
     }
 
+    public function getInvoiceAlreadyCreated($invoiceTitle, Octo\System\Model\Contact $contact, $useConnection = 'read')
+    {
+        if (is_null($invoiceTitle) || is_null($contact)) {
+            throw new StoreException('Value passed to ' . __FUNCTION__ . ' cannot be null.');
+        }
+
+        $query = new Query($this->getNamespace('Invoice').'\Model\Invoice', $useConnection);
+        $query->select('*')->from('invoice')->limit(1);
+        $query->where('`title` = :title AND contact_id = :contact_id AND invoice_status_id=1 AND total_paid IS NULL');
+        $query->bind(':title', $invoiceTitle);
+        $query->bind(':contact_id', $contact->getId());
+
+        try {
+            $query->execute();
+            return $query->fetch();
+        } catch (PDOException $ex) {
+            throw new StoreException('Could not get Invoice by Uuid', 0, $ex);
+        }
+    }
+
 
 
 }
