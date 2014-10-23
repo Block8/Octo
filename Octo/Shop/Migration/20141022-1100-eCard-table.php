a4a022c466fb43f40bcdb3ab2b82316f21fc99db
Leszek Pietrzak <leszek.pietrzak@re-systems.co.uk>
2014-10-23 12:22:57 +0100

    new version of ecard table

--- Octo/Shop/Migration/20141022-1100-eCard-table.php
+++ Octo/Shop/Migration/20141022-1100-eCard-table.php
@@ -3,12 +3,17 @@ $queries = [];
 
 $queries[] = "CREATE TABLE `ecard` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `invoice_id` int(11) unsigned NOT NULL,
+  `item_id` int(11) unsigned NOT NULL,
   `message` text,
   `to_email` varchar(12800) DEFAULT NULL,
   `from_email` varchar(128) DEFAULT NULL,
-  `item_id` int(11) unsigned DEFAULT NULL,
+  `send_statuses` varchar(2000) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
+  KEY `invoice_id` (`invoice_id`),
   KEY `item_id` (`item_id`),
-  CONSTRAINT `ecard_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `item` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+  CONSTRAINT `ecard_ibfk_1` FOREIGN KEY (`invoice_id`) REFERENCES `invoice` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `ecard_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `item` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
