918f0a4eac4f58b991d976ed009c955b4d9a215a
Dan Cryer <dan@block8.co.uk>
2014-11-25 13:08:46 +0000

    Reset password functionality. Fixes #4

--- Octo/System/Admin/Controller/SessionController.php
+++ Octo/System/Admin/Controller/SessionController.php
@@ -71,33 +71,66 @@ class SessionController extends Controller
         die;
     }
 
-    public function reset()
+    public function forgotPassword()
     {
+        if ($this->getRequest()->getMethod() == 'POST') {
+            $email = $this->getParam('email');
+            $user = $this->userStore->getByEmail($email);
 
-//        $recipients = array_filter(explode("\n", $form->getRecipients()));
-//
-//        $to = implode(', ', $recipients);
-//
-//        $subject = 'Form Submission: ' . $form->getTitle();
-//        $headers = array(
-//            'MIME-Version: 1.0',
-//            'Content-type: text/html; charset=utf-8',
-//        );
-//
-//        if ($submission->getContact() && $submission->getContact()->getEmail()) {
-//            $headers[] = 'Reply-To: ' . $submission->getContact()->getEmail();
-//        }
-//
-//        $config = Config::getInstance();
-//
-//        if (isset($config->site['email_from'])) {
-//            $headers[] = 'From: ' . $config->site['email_from'];
-//        }
-//
-//        $message         = Template::getPublicTemplate('Emails/FormSubmission');
-//        $message->form   = $form;
-//        $message->submission = $submission;
-//
-//        mail($to, $subject, $message->render(), implode("\r\n", $headers));
+            if (is_null($user)) {
+                $this->view->error = 'No user exists with that email address, please try again.';
+                return;
+            }
+
+            $userId = $user->getId();
+            $key = md5(date('Y-m-d') . $user->getHash());
+            $name = $user->getName();
+            $siteName = $this->config->get('site.name');
+            $url = $this->config->get('site.url') . '/' . $this->config->get('site.admin_uri') . '/';
+
+            $message = <<<OUT
+Dear {$name},
+
+You have received this email because you, or someone else, has requested a password reset for the {$siteName} admin system.
+
+If this was you, please click the following link to reset your password: {$url}session/reset-password/{$userId}/{$key}
+
+Otherwise, please ignore this email and no action will be taken.
+
+Thank you,
+{$siteName}
+OUT;
+
+            $to = $name . ' <' . $user->getEmail() . '>';
+            @mail($to, $siteName . ' Password Reset', $message, 'From: ' . $this->config->get('site.email_from'));
+            $this->view->emailed = true;
+        }
+    }
+
+    public function resetPassword($userId, $key)
+    {
+        $user = $this->userStore->getById($userId);
+        $userKey = md5(date('Y-m-d') . $user->getHash());
+
+        if (empty($user) || $key != $userKey) {
+            $this->view->error = 'Invalid password reset request.';
+            return;
+        }
+
+        if ($this->request->getMethod() == 'POST') {
+            $hash = password_hash($this->getParam('password'), PASSWORD_DEFAULT);
+            $user->setHash($hash);
+            $this->userStore->save($user);
+
+            $_SESSION['user_id'] = $user->getId();
+
+            header('Location: ' . $this->config->get('site.url') . '/' . $this->config->get('site.admin_uri'));
+            die;
+        }
+
+        $this->view->userId = $userId;
+        $this->view->key = $key;
+
+        return;
     }
 }
