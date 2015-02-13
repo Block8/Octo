8bae3d20d07fadf9b0cfd3220ff91bee3b910c56
Dan Cryer <dan@block8.co.uk>
2015-02-13 13:57:04 +0000

    Moving Pages to an external module.

--- Octo/Pages/Migration/20140307-1448.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$queries = [
-    "ALTER TABLE `page_version` ADD `image_id` CHAR(32)  NULL  DEFAULT NULL  AFTER `template`;",
-    "ALTER TABLE `page_version` CHANGE `image_id` `image_id` CHAR(32)  CHARACTER SET utf8  NULL  DEFAULT NULL;",
-    "ALTER TABLE `page_version` ADD FOREIGN KEY (`image_id`)
-    REFERENCES `file` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;",
-];
--- Octo/Pages/Migration/20140327-1358.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$queries = [
-    "ALTER TABLE `page` ADD `position` INT  NOT NULL  DEFAULT '0'  AFTER `uri`;"
-];
